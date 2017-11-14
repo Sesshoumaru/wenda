84f76bd8e71d16d14470cf778a22a3d7862e5acb
Sesshoumaru <laobi.net@live.com>
2017-11-14 23:14:48 +0800

    导航条

--- wenda.py
+++ wenda.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask,render_template
 import config
 
 app = Flask(__name__)
 app.config.from_object(config)
 
 @app.route("/")
-def hello_world():
-    return "hello world!"
+def index():
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
