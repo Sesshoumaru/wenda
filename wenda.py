66980768d06f9bee5702dbd96ce6c2189cb3c948
Sesshoumaru <laobi.net@live.com>
2017-11-14 23:56:07 +0800

    登录页面html

--- wenda.py
+++ wenda.py
@@ -1,12 +1,21 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, request, url_for
 import config
 
 app = Flask(__name__)
 app.config.from_object(config)
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:
+        pass
 
 
 if __name__ == "__main__":
