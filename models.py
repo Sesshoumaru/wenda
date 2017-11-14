a9ae89fed6b9c7e74222bea7bacb9a8e2d146362
Sesshoumaru <laobi.net@live.com>
2017-11-15 00:39:19 +0800

    User模型创建

---
+++ models.py
@@ -0,0 +1,10 @@
+from extensions import db
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    telephone = db.Column(db.String(11), nullable=False)
+    username = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
--- modes.py
+++
@@ -1,2 +0,0 @@
-from extensions import db
-
