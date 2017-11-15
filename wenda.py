1c94fde642e68d1c62568ede8c7a2fc8b3b0747e
Sesshoumaru <laobi.net@live.com>
2017-11-15 13:50:38 +0800

    注册和登录功能

--- wenda.py
+++ wenda.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for, redirect, session
 import config
+from extensions import db
+from models import User
 
 app = Flask(__name__)
 app.config.from_object(config)
+db.init_app(app)
 
 
 @app.route("/")
@@ -15,7 +18,16 @@ def login():
     if request.method == 'GET':
         return render_template('login.html')
     else:
-        pass
+        telephone = request.form.get("telephone")
+        password = request.form.get("password")
+
+        user = User.query.filter(User.telephone == telephone, User.password == password).first()
+        if not user:
+            return "用户名不存在，或者密码错误!"
+        else:
+            session["user_id"] = user.id
+            session.permanent = True
+            return redirect(url_for('index'))
 
 
 @app.route("/register", methods=['GET', 'POST'])
@@ -23,8 +35,34 @@ def register():
     if request.method == 'GET':
         return render_template('register.html')
     else:
-        pass
+        username = request.form.get("username")
+        telephone = request.form.get("telephone")
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
 
+        user = User.query.filter(User.telephone == username).first()
+        if user:
+            return "该手机号码已注册，请更换手机号码!"
+        elif password1 != password2:
+            return "两次输入的密码不一致，请重新输入!"
+        else:
+            user = User(username=username, telephone=telephone, password=password1)
+            db.session.add(user)
+            db.session.commit()
+            return redirect(url_for('login'))
+
+        return render_template('register.html')
+
+
+@app.context_processor
+def my_context_processor():
+    user_id = session.get("user_id")
+    if user_id:
+        user = User.query.filter(User.id == user_id).first()
+        if user:
+            return {"user":user}
+
+    return {}
 
 if __name__ == "__main__":
     app.run()
